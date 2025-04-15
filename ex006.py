6e55d87fe98204a9cc38969f859797c51215ecc0
kauanrosa001 <kauanrosa764@gmail.com>
2025-04-14 21:07:26 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um numero: '))
+m = n * 2
+m2 = n * 3
+r = n **(1/2)
+print('o drobro de {} é {} \n o triplo de {} é {} \n e a raiz quadrada é {:.2f} '.format(n , m , n , m2 , r))
