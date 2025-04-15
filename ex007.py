4b341251707942c82f5d74d42dec1350890074d5
kauanrosa001 <kauanrosa764@gmail.com>
2025-04-14 21:07:37 -0300

    Add files via upload

---
+++ ex007.py
@@ -0,0 +1,5 @@
+n1 = float(input('Primeira nota do aluno:'))
+n2 = float(input('Segunda nota do aluno:'))
+n3 = float(input('Terceira nota do aluno:'))
+m = (n1 + n2 + n3) /2
+print('A media das notas sera {}'.format(m,n1 , n2 , n3))
